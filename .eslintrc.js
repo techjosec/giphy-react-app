8ce652f431ba0a86caaa3a0f8b3bdc9c00da79e3
techjosec <tec.josec@gmail.com>
2021-02-27 15:50:05 -0300

    eslint conig uptared

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
+const RULES = {
+
+	ERROR : `error`,
+	OFF   : `off`,
+	ON    : `on`,
+
+};
+
+const { ERROR, OFF } = RULES;
+
 module.exports = {
 	env: {
 		browser : true,
@@ -18,27 +28,27 @@ module.exports = {
 		`react`,
 	],
 	rules: {
-		'brace-style'                       : [`error`, `allman`],
-		'consistent-return'                 : `error`,
-		'import/no-extraneous-dependencies' : [`error`, { devDependencies: true }],
-		indent                              : [`error`, `tab`],
-		'key-spacing'                       : [`error`, {
+		'brace-style'                       : [ERROR, `allman`],
+		'consistent-return'                 : ERROR,
+		'import/no-extraneous-dependencies' : [ERROR, { devDependencies: true }],
+		indent                              : [ERROR, `tab`],
+		'key-spacing'                       : [ERROR, {
 			align: {
 				beforeColon : true,
 				afterColon  : true,
 				on          : `colon`,
 			},
 		}],
-		'linebreak-style'              : [`off`],
-		'no-underscore-dangle'         : [`off`],
-		'no-mixed-spaces-and-tabs'     : [`off`],
-		'no-tabs'                      : [`error`, { allowIndentationTabs: true }],
-		'no-unused-vars'               : [`error`, { argsIgnorePattern: `^_` }],
-		quotes                         : [`error`, `backtick`],
-		'react/jsx-indent-props'       : [`off`],
-		'react/forbid-prop-types'      : [`off`],
+		'linebreak-style'              : [OFF],
+		'no-underscore-dangle'         : [OFF],
+		'no-mixed-spaces-and-tabs'     : [OFF],
+		'no-tabs'                      : [ERROR, { allowIndentationTabs: true }],
+		'no-unused-vars'               : [ERROR, { argsIgnorePattern: `^_` }],
+		quotes                         : [ERROR, `backtick`],
+		'react/jsx-indent-props'       : [OFF],
+		'react/forbid-prop-types'      : [OFF],
 		'react/jsx-filename-extension' : [1, { extensions: [`.js`, `.jsx`] }],
-		'react/jsx-indent'             : [`off`],
-		'space-in-parens'              : [`error`, `always`],
+		'react/jsx-indent'             : [OFF],
+		'space-in-parens'              : [ERROR, `always`],
 	},
 };
