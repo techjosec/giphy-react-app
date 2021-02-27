7acd3910220af3c8bb8b9c535a1eb370ec8828da
techjosec <tec.josec@gmail.com>
2021-02-27 16:35:21 -0300

    material-ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
 
-function App()
+const App = () =>
 {
+	const [gifs, setGifs] = useState( [] );
+
+	console.log( gifs );
+
 	return (
 		<div className="App">
-			<header className="App-header" />
+			<section className="App-content">
+				<h3>Another Giphy React APP!</h3>
+				<Button variant="contained" color="primary">
+      				Hola Mundo!
+				</Button>
+			</section>
 		</div>
 	);
-}
+};
 
 export default App;
